822123b1afee798af247a4e97f6e496971468735
PeterCen <p.centurion@hotmail.com>
2024-08-13 18:11:33 -0300

    Adicionando os primeiros documentos das aulas de typescrupt.

---
+++ Formação Angular Developer/TypeScript para Angular/src/index.js
@@ -0,0 +1,8 @@
+function printaObjeto(pessoa) {
+    console.log(pessoa);
+}
+printaObjeto({
+    name: "Bruce Wayne",
+    vulgo: "Batman"
+});
+console.log("Olá Mundo!");
---
+++ Formação Angular Developer/TypeScript para Angular/src/index.ts
@@ -0,0 +1,19 @@
+//Esse documento serve para desenvolver, mas oque iremos executar será
+//o documento index.js, com o comando de tradução :npx tsc src/index.ts
+//E rodando ele com: node src/index.ts
+
+type heroi={
+    name: string;
+    vulgo: string;
+};
+
+function printaObjetos(pessoa: heroi){
+    console.log(pessoa);
+}
+
+printaObjetos({
+    name: "Bruce Wayne",
+    vulgo: "Batman"
+})
+
+console.log("Olá Mundo!");
