89e764b9909870ef688a31be23ebc2f2b101b154
PeterCen <p.centurion@hotmail.com>
2024-08-14 09:44:52 -0300

    Feita a remoção dos arquivos testes das primeiras aulas, e adicionado um script no arquivo package.json.

---
+++ Formação Angular Developer/TypeScript para Angular/build/index.js
@@ -0,0 +1,12 @@
+"use strict";
+//Esse documento serve para desenvolver, mas oque iremos executar será
+//o documento index.js, com o comando de tradução :npx tsc src/index.ts
+//E rodando ele com: node src/index.ts
+function printaObjetos(pessoa) {
+    console.log(pessoa);
+}
+printaObjetos({
+    name: "Bruce Wayne",
+    vulgo: "Batman"
+});
+console.log("Olá Mundo!");
--- Formação Angular Developer/TypeScript para Angular/src/caseComJS.js
+++
@@ -1,9 +0,0 @@
-function ligar(herio){
-    console.log("Ligando para: "+herio.telefone);
-}
-
-ligar({
-    nome: "Steve Rogers",
-    vulgo: "Capitão América",
-    //telefone: "11 31713388",
-})
